9a157e00955fad5eda0ce189ff097991295322ae
Brandon Jank <jankster@gmail.com>
2014-10-25 22:08:48 -0700

    Derp

--- rotations/druid/restoration.lua
+++ rotations/druid/restoration.lua
@@ -60,14 +60,14 @@ PossiblyEngine.rotation.register_custom(105, "bbDruid Restoration", {
   { "Swiftmend", { "lowest.health < 80", "lowest.buff(Regrowth)" }, "lowest" },
   { "Rejuvination", { "!lowest.buff(Rejuvination)", "player.mana > 80" }, "lowest" },
   { "Rejuvination", { "!lowest.buff(Rejuvination)", "lowst.health < 95" }, "lowest" },
-  { "Regrowth", { "lowest.health < 60", "!lowest.buff(Regrowth)" }, "lowest" } -- when no instant cast spells are available,  if Glyph of Regrowth, then replaces Healing Touch
-  { "Healing Touch", "player.health < 60", "player" }
+  { "Regrowth", { "lowest.health < 60", "!lowest.buff(Regrowth)" }, "lowest" }, -- when no instant cast spells are available,  if Glyph of Regrowth, then replaces Healing Touch
+  { "Healing Touch", "player.health < 60", "player" },
   { "Healing Touch", "lowest.health < 60", "lowest" },
   { {
     { "Swiftmend", true, "lowest"},
     { "Regrowth", true, "lowest"},
     { "Healing Touch", true, "lowest"},
-  },{ "player.buff(Harmony) < 3", "!lowest.range > 40" },
+  }, "player.buff(Harmony) < 3", "!lowest.range > 40" },
 
 }, {
 -- OUT OF COMBAT ROTATION
