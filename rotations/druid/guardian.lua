fd4260ac4df693780f9eb080bca961c2f213202f
Brandon Jank <jankster@gmail.com>
2014-10-30 14:46:09 -0700

    g4

--- rotations/druid/guardian.lua
+++ rotations/druid/guardian.lua
@@ -32,8 +32,8 @@ PossiblyEngine.rotation.register_custom(104, "bbDruid Guardian", {
 	{ "Bear Form", { "!player.form = 1", "target.exists", "target.distance < 5" } },
 
 	-- INTERRUPTS
-	--{ "Skull Bash", "target.interrupt" },
-	--{ "Mighty Bash", { "talent(5, 3)", "target.interrupt" } },
+	{ "Skull Bash", "target.interrupt" },
+	{ "Mighty Bash", { "talent(5, 3)", "target.interrupt" } },
 
 	-- DREAM PROCS
 	--{ "Healing Touch", { "player.health < 90", "player.buff(Dream of Cenarius)" }, "player" },
