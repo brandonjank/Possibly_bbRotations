934a3884d19e67f8662b4127faeacc291c4c0e20
Brandon Jank <jankster@gmail.com>
2014-11-04 03:42:31 -0800

    Update Windwalker Monk for WoD

--- rotations/monk/windwalker.lua
+++ rotations/monk/windwalker.lua
@@ -1,135 +1,83 @@
--- PossiblyEngine Rotation Packager
--- Custom Protection Warrior Rotation
--- Created on Dec 25th 2013 1:00 am
-PossiblyEngine.rotation.register_custom(269, "bbWindwalkerMonk", {
--- PLAYER CONTROLLED:
--- SUGGESTED TALENTS:
+-- PossiblyEngine Rotation
+-- Windwalker Monk - WoD 6.0.3
+-- Updated on Nov 4th 2014
+
+-- PLAYER CONTROLLED: Paralysis
+-- TALENTS: Tiger's Lust, Chi Wave, Chi Brew, Leg Sweep, Diffuse Magic, Invoke Xuen, the White Tiger
+-- GLYPHS: Major: Glyph of Touch of Karma, Glyph of Touch of Death, Glyph of Transcendence  Minor: Glyph of Flying Serpent Kick
 -- CONTROLS: Pause - Left Control
 
+PossiblyEngine.rotation.register_custom(269, "bbMonk Windwalker", {
 -- COMBAT
 	-- Rotation Utilities
 	{ "pause", "modifier.lcontrol" },
-	{ "pause", "@bbLib.bossMods" },
-	{ "pause", { "toggle.pvpmode", "@bbLib.BGFlag" } },
+
+	-- AUTO TARGET
 	{ "/script TargetNearestEnemy()", { "toggle.autotarget", "!target.exists" } },
 	{ "/script TargetNearestEnemy()", { "toggle.autotarget", "target.exists", "target.dead" } },
 
-	-- Racials
-	{ "Will of the Forsaken", "player.state.fear" },
-	{ "Will of the Forsaken", "player.state.charm" },
-	{ "Will of the Forsaken", "player.state.sleep" },
-	{ "Quaking Palm", "modifier.interrupts" },
-
-	-- Off GCD
-	{ "Touch of Death", "player.buff(Death Note)" },
-
 	-- Interrupts
-	{ "Spear Hand Strike", "modifier.interrupts" },
-	{ "Grapple Weapon", "modifier.interrupts" },
-	{ "Leg Sweep", "modifier.interrupts", "target.range <= 5" },
+	{ "Spear Hand Strike", "modifier.interrupt" },
+	{ "Leg Sweep", { "talent(4, 3)", "modifier.interrupt", "target.range <= 5" } },
 
-	-- Survival
-	{ "Fortifying Brew", "player.health <= 30" },
-	{ "Touch of Karma", "player.health <= 50" },
+-- DEFENSIVE COOLDOWNS
 	{ "Nimble Brew", "player.state.fear" },
 	{ "Nimble Brew", "player.state.stun" },
 	{ "Nimble Brew", "player.state.root" },
 	{ "Nimble Brew", "player.state.horror" },
-	{ "Dampen Harm", "player.health <= 45" },
-	{ "Diffuse Magic", "player.health <= 45" },
+	{ "Touch of Karma", { "player.health < 50", "target.range < 2" } },
+	{ "Fortifying Brew", "player.health < 30" },
+	{ "Zen Meditation", { "player.health < 20", "target.range > 3" } },
+	{ "Diffuse Magic", { "talent(5, 3)", "player.health < 90", (function() return UnitIsUnit('targettarget', 'player') end), "target.casting.time > 0" } },
 
-	-- Keybinds
-	{ "Paralysis", "modifier.shift", "mouseover" },
-	{ "Healing Sphere", "modifier.alt", "ground" },
-	{ "Crackling Jade Lightning", "modifier.control", "target" },
+-- DPS COOLDOWNS
+	{ "Chi Brew", { "talent(3, 3)", "player.chi < 3", "player.spell(Chi Brew).charges == 1", "player.spell(Chi Brew).recharge <= 10" } },
+	{ "Chi Brew", { "talent(3, 3)", "player.chi < 3", "player.spell(Chi Brew).charges == 2" } },
+	{ "Chi Brew", { "talent(3, 3)", "player.chi < 3", "target.deathin < 15" } },
+	{ "Invoke Xuen, the White Tiger", "talent(6, 2)" },
+	{ "Energizing Brew", { "player.energy < 40", "player.timetomax > 6" } },
+	{ "Tigereye Brew", "player.buff(Tigereye Brew).count > 9" },
+	{ "Touch of Death", "player.buff(Death Note)" },
 
+-- MULTI TARGET 4+
+	{{
+		{ "Rising Sun Kick", "!target.debuff(Rising Sun Kick)" },
+		{ "Rising Sun Kick", "target.debuff(Rising Sun Kick).duration < 3" },
+		{ "Tiger Palm", "!player.buff(Tiger Power)"  },
+		{ "Tiger Palm", "player.buff(Tiger Power).duration <= 2" },
+		{ "Fists of Fury", { "player.buff(Tiger Power).duration > 4", "target.debuff(Rising Sun Kick).duration > 4" } },
+		{ "Spinning Crane Kick", "player.chi < 4" },
+		-- TODO: To effectively use Storm, Earth, and Fire you need to be facing 2-3 targets with an equal DPS priority, expect the targets to live for 10 or more seconds, and be able to remain in range of the targets at all times.
+	},{
+		"player.area(8).enemies > 3",
+	},
 
+-- SINGLE TARGET
+	-- CHI BUILDERS
+	{ "Expel Harm", { "player.chi < 4", "player.health < 80" },
+	{ "Jab", "player.chi < 4" }
 
-	{ "Tiger's Lust", "target.range >= 15" },
-
--- Chi Builders
-	{ "Expel Harm", "player.health < 80" },
--- actions+=/rushing_jade_wind,if=talent.rushing_jade_wind.enabled
-	{ "Rushing Jade Wind", "modifier.multitarget" },
-
-
--- PRE COMBAT
--- flask,type=spring_blossoms
--- food,type=sea_mist_rice_noodles
--- stance,choose=fierce_tiger
--- snapshot_stats
--- virmens_bite_potion
-
--- COMBAT
--- auto_attack
--- chi_sphere,if=talent.power_strikes.enabled&buff.chi_sphere.react&chi<4
-	{ "Chi Sphere", { "player.spell(Power Strikes).exists", "player.buff(Chi Sphere)", "player.chi < 4" } },
--- virmens_bite_potion,if=buff.bloodlust.react|target.time_to_die<=60
-	{ "#76089", { "target.exists", "target.deathin > 15", "@bbLib.useAgiPot" } }, -- Agility Potion (76089)
--- use_item,name=gloves_of_the_golden_protector
--- berserking
-	{ "Berserking", { "modifier.cooldowns", "!@bbLib.playerHasted" } },
--- chi_brew,if=talent.chi_brew.enabled&chi<=2&(trinket.proc.agility.react|(charges=1&recharge_time<=10)|charges=2|target.time_to_die<charges*10)
-	--{ "Chi Brew", { "player.chi < 3", } }, -- TODO: trinket.proc.agility.react
-	{ "Chi Brew", { "player.chi < 3", "player.spell(Chi Brew).charges == 1", "player.spell(Chi Brew).recharge <= 10" } },
-	{ "Chi Brew", { "player.chi < 3", "player.spell(Chi Brew).charges == 2" } },
-	{ "Chi Brew", { "player.chi < 3", "target.deathin < 15" } },
--- tiger_palm,if=buff.tiger_power.remains<=3
-	{ "Tiger Palm", "player.buff(Tiger Power).duration <= 3" },
--- tigereye_brew,if=buff.tigereye_brew_use.down&buff.tigereye_brew.stack=20
-	{ "Tigereye Brew", "player.buff(Tigereye Brew).count == 20" },
--- tigereye_brew,if=buff.tigereye_brew_use.down&trinket.proc.agility.react
--- tigereye_brew,if=buff.tigereye_brew_use.down&chi>=2&(trinket.proc.agility.react|trinket.proc.strength.react|buff.tigereye_brew.stack>=15|target.time_to_die<40)&debuff.rising_sun_kick.up&buff.tiger_power.up
--- energizing_brew,if=energy.time_to_max>5
-	{ "Energizing Brew", "player.timetomax > 5" },
--- rising_sun_kick,if=debuff.rising_sun_kick.down
-	{ "Rising Sun Kick", "target.debuff(Rising Sun Kick).duration <= 3" },
--- tiger_palm,if=buff.tiger_power.down&debuff.rising_sun_kick.remains>1&energy.time_to_max>1
-	{ "Tiger Palm", { "player.buff(Tiger Power).count < 3", "target.debuff(Rising Sun Kick).duration > 1", "player.timetomax > 1" } },
--- invoke_xuen,if=talent.invoke_xuen.enabled
-	{ "Invoke Xuen: The White Tiger", "modifier.cooldowns" },
-
--- MULTI
--- actions.aoe=rising_sun_kick,if=chi=4
-	{ "Rising Sun Kick", { "modifier.multitarget", "player.chi > 3" } },
--- actions.aoe+=/spinning_crane_kick
-	{ "Spinning Crane Kick", "modifier.multitarget" },
-
--- SINGLE
--- rising_sun_kick
-	{ "Rising Sun Kick" },
--- fists_of_fury,if=buff.energizing_brew.down&energy.time_to_max>4&buff.tiger_power.remains>4
-	{ "Fists of Fury", { "!player.buff(Energizing Brew)", "player.timetomax > 4", "player.buff(Tiger Power).duration > 4" } },
--- chi_wave,if=talent.chi_wave.enabled&energy.time_to_max>2
-	{ "Chi Wave", "player.timetomax > 2" },
--- chi_burst,if=talent.chi_burst.enabled&energy.time_to_max>2
-	{ "Chi Burst", "player.timetomax > 2" },
--- zen_sphere,cycle_targets=1,if=talent.zen_sphere.enabled&energy.time_to_max>2&!dot.zen_sphere.ticking
-	{ "Zen Sphere", { "!target.debuff(Zen Sphere)", "player.timetomax > 2" }, "target" },
--- blackout_kick,if=buff.combo_breaker_bok.react
+	-- CHI FINISHERS
+	{ "Tiger Palm", "!player.buff(Tiger Power)"  },
+	{ "Tiger Palm", "player.buff(Tiger Power).duration <= 2" },
+	{ "Rising Sun Kick", "!target.debuff(Rising Sun Kick)" },
+	{ "Rising Sun Kick", "target.debuff(Rising Sun Kick).duration < 3" },
+	{ "Fists of Fury", { "player.buff(Tiger Power).duration > 4", "target.debuff(Rising Sun Kick).duration > 4" } },
 	{ "Blackout Kick", "player.buff(Combo Breaker: Blackout Kick)" },
--- tiger_palm,if=buff.combo_breaker_tp.react&(buff.combo_breaker_tp.remains<=2|energy.time_to_max>=2)
-	{ "Tiger Palm", { "player.buff(Combo Breaker: Tiger Palm)", "player.buff(Combo Breaker: Tiger Palm).duration <= 2" } },
-	{ "Tiger Palm", { "player.buff(Combo Breaker: Tiger Palm)", "player.timetomax >= 2" } },
--- jab,if=chi.max-chi>=2
-	{ "Jab", "player.chi <= 3" }, --TODO: chi.max-chi>=2
--- blackout_kick,if=energy+energy.regen*cooldown.rising_sun_kick.remains>=40
-	{ "Blackout Kick", { "player.chi >= 2", "target.debuff(Rising Sun Kick)", "player.buff(Tiger Power)"} },
-
+	{ "Tiger Palm", "player.buff(Combo Breaker: Tiger Palm)" },
+	{ "Chi Wave", { "talent(2, 1)", "player.timetomax > 2" } },
+	{ "Blackout Kick" },
 
 },{
 -- OUT OF COMBAT
-  	-- Pause
+  -- PAUSE
 	{ "pause", "modifier.lcontrol" },
 
+	-- OOC HEAL
 	{ "Expel Harm", "player.health < 80" },
-	{ "Fortifying Brew", "player.health <= 30" },
-	{ "Touch of Karma", "player.health <= 50" },
 
 	-- Keybinds
-	{ "Paralysis", "modifier.shift", "mouseover" },
-	{ "Healing Sphere", "modifier.alt", "ground" },
-	{ "Crackling Jade Lightning", "modifier.control", "target" },
+	--{ "Crackling Jade Lightning", "modifier.control", "target" },
 
 	-- Buffs
 	{ "Legacy of the White Tiger", "!player.buff(Legacy of the White Tiger)" },
