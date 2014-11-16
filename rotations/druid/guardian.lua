f93acb78db4778a1c282276025a71ab999d631f9
Backburn <backburn@hotmail.com>
2014-11-15 21:36:15 -0800

    Bear 2.0

--- rotations/druid/guardian.lua
+++ rotations/druid/guardian.lua
@@ -7,7 +7,7 @@
 -- GLYPHS: Glyph of Maul, Glyph of Rebirth, Glyph of Fae Silence, Glyph of Grace (minor)
 -- CONTROLS: Pause - Left Control
 
-PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |cFFFF9966Druid Guardian", {
+PossiblyEngine.rotation.register_custom(104, "bbDruid Guardian", {
 -- COMBAT ROTATION
 	-- PAUSE
 	{ "pause", "modifier.lcontrol" },
@@ -25,11 +25,8 @@ PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |c
 		"toggle.frogs",
 	} },
 
-	-- BATTLE REZ
-	{ "Rebirth", { "target.friend", "target.dead", "!player.form = 1" }, "target" },
-
 	-- BEAR FORM
-	{ "Bear Form", "!player.form = 1" }, -- force in raid only
+	{ "Bear Form", "!player.buff(Bear Form)" },
 
 	-- INTERRUPTS
 	{ "Skull Bash", "modifier.interrupt" },
@@ -40,10 +37,7 @@ PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |c
 	-- DREAM PROCS
 	{ {
 		{ "Remove Corruption", { "!modifier.last", "player.dispellable" }, "player" },
-		{ "Rebirth", { "target.friend", "target.dead" }, "target" },
-		{ "Healing Touch", "player.health < 90", "player" },
-		{ "Healing Touch", "targettarget.health < 90", "targettarget" },
-		{ "Healing Touch", "lowest.health < 60", "lowest" },
+		{ "Rebirth", { "target.exists", "target.friend", "target.dead" }, "target" },
 	},{
 		"player.buff(Dream of Cenarius)",
 	} },
@@ -52,32 +46,39 @@ PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |c
 	{ "Faerie Fire", { "target.exists", "target.distance > 5" } },
 	{ "Faerie Fire", { "toggle.mouseovers", "mouseover.alive", "mouseover.enemy", "mouseover.distance > 5" }, "mouseover" },
 
-	-- DEFENSIVE CONSUMABLES
+	-- DEFENSIVE COOLDOWNS
 	{ "#89640", { "toggle.consume", "player.health < 40", "!player.buff(130649)", "target.boss" } }, -- Life Spirit (130649)
 	{ "#5512", { "toggle.consume", "player.health < 40" } }, -- Healthstone (5512)
 	{ "#76097", { "toggle.consume", "player.health < 20", "target.boss" } }, -- Master Healing Potion (76097)
+	{ "Survival Instincts", { "target.enemy", "target.alive", "player.health < 50" } },
+	{ "Survival Instincts", { "target.enemy", "target.alive", "player.health < 80", "player.spell(Survival Instincts).charges > 1" } },
 
-	-- DEFENSIVE COOLDOWNS
-	{ "Frenzied Regeneration", { "player.level >= 68", "!modifier.last", "player.health < 60" } },
-	{ "Survival Instincts", { "player.level >= 56", "player.health < 50" } },
-	{ "Savage Defense", { "player.rage >= 60", "target.distance < 5", "!player.buff(Savage Defense)", "player.health <= 80" } }, --"timeout(Savage Defense, 12)"
-	{ "Barkskin", { "player.health < 90", "target.distance < 5" } },
-
-	-- DPS COOLDOWNS
-	{ "Berserking", { "!player.hashero", "target.distance < 5" } },
-	{ "Berserk", { "player.hashero", "!player.buff(Incarnation: Son of Ursoc)" } },
-	{ "Berserk", { "target.boss", "!player.buff(Incarnation: Son of Ursoc)" } },
-
-	-- THREAT ROTATION -- need a minimum for 60 range for savage defense
-	{ "Mangle" },
-	{ "Thrash", "!target.debuff(Thrash)" },
-	{ "Thrash", { "target.debuff(Thrash)", "target.debuff(Thrash).duration <= 1" } },
-	{ "Lacerate", "!target.debuff(Lacerate)" },
-	{ "Lacerate", "target.debuff(Lacerate).duration <= 3" },
-	{ "Lacerate", "target.debuff(Lacerate).count < 3" },
+	-- THREAT ROTATION
+	{ "Savage Defense", { "modifier.cooldowns", "player.rage >= 60", "target.distance < 5", "target.agro", "!player.buff(Savage Defense)", "player.health <= 80" } },
+	{ "Blood Fury", { "modifier.cooldowns", "target.enemy", "target.alive" } },
+	{ "Berserking", { "modifier.cooldowns", "target.enemy", "target.alive" } },
+	{ "Arcane Torrent", "player.rage <= 75" },
+	-- actions+=/use_item,slot=trinket1
+	{ "Barkskin", { "player.health < 90", "target.agro", "target.distance < 5" } },
 	{ "Maul", { "player.rage > 79", "player.buff(Tooth and Claw)", "player.spell(Mangle).cooldown > 0.5" } },
 	{ "Maul", "player.rage > 94" },
-	{ "Thrash", { "player.spell(Mangle).cooldown > 0.5", "timeout(Thrash, 1)", "target.area(8).enemies > 2" } },
+	{ "Berserk", { "modifier.cooldowns", "player.buff(Pulverize).remains > 10" } },
+	{ "Frenzied Regeneration", { "player.rage >= 80", "!modifier.last", "player.health < 60" } },
+	{ "Cenarion Ward" },
+	{ "Renewal", "player.health < 30" },
+	{ "Heart of the Wild", { "modifier.cooldowns", "player.health < 95", "target.agro", "target.distance < 5" } },
+	{ "Rejuvenation", { "player.buff(Heart of the Wild)", "player.buff(Heart of the Wild).remains <= 4" } },
+	{ "Nature's Vigil", { "modifier.cooldowns", "player.health < 95", "target.agro", "target.distance < 5" } },
+	{ "Healing Touch", { "player.buff(Dream of Cenarius)", "player.health < 60" }, "player" },
+	{ "Pulverize", "player.buff(Pulverize).remains < 0.5" },
+	-- actions+=/lacerate,if=talent.pulverize.enabled&buff.pulverize.remains<=(3-dot.lacerate.stack)*gcd&buff.berserk.down
+	{ "Incarnation: Son of Ursoc", { "modifier.cooldowns", "player.health < 70", "target.agro", "target.distance < 5" } },
+	{ "Lacerate", "target.debuff(Lacerate).remains <= 3" },
+	{ "Thrash", "!target.debuff(Thrash)" },
+	{ "Mangle" },
+	{ "Thrash", { "target.debuff(Thrash)", "target.debuff(Thrash).duration <= 1" } },
+	{ "Lacerate" },
+
 
 },{
 -- OUT OF COMBAT ROTATION
@@ -90,11 +91,7 @@ PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |c
 	{ "Mark of the Wild", { "!player.buffs.stats", "lowest.distance <= 30" }, "lowest" },
 
 	-- REZ
-	{ "Revive", { "target.exists", "target.dead", "!player.moving", "target.player" }, "target" },
-
-	-- HEAL
-	{ "Rejuvenation", { "!player.ininstance", "player.health < 70", "!player.buff(Rejuvenation)" }, "player" },
-	{ "Healing Touch", { "!player.ininstance", "player.health < 50", "!player.moving)" }, "player" },
+	{ "Revive", { "target.exists", "target.dead", "target.player", "!player.moving" }, "target" },
 
 	-- FROGGING
 	{ {
@@ -119,6 +116,6 @@ PossiblyEngine.rotation.register_custom(104, "|cFFFF0000bb|cFF0000FFRotations |c
 function()
 	PossiblyEngine.toggle.create('consume', 'Interface\\Icons\\inv_alchemy_endlessflask_06', 'Use Consumables', 'Toggle the usage of Flasks/Food/Potions etc..')
 	PossiblyEngine.toggle.create('autotarget', 'Interface\\Icons\\ability_hunter_snipershot', 'Auto Target', 'Automatically target the nearest enemy when target dies or does not exist.')
-	PossiblyEngine.toggle.create('mouseovers', 'Interface\\Icons\\ability_hunter_quickshot', 'Use Mouseovers', 'Toggle automatic usage of stings/scatter/etc on eligible mouseover targets.')
-	PossiblyEngine.toggle.create('frogs', 'Interface\\Icons\\inv_misc_fish_33', 'Gulp Frog Mode', 'Automaticly target and follow Gulp Frogs.')
+	PossiblyEngine.toggle.create('mouseovers', 'Interface\\Icons\\ability_hunter_quickshot', 'Use Mouseovers', 'Toggle usage of mouseover Faerie Fire.')
+	PossiblyEngine.toggle.create('frogs', 'Interface\\Icons\\inv_misc_fish_33', 'Gulp Frog Mode', 'Automaticly target and attack Gulp Frogs.')
 end)
